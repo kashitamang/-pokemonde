3c22bf18c259a63f115ac4d7bab7de6e29efc570
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-23 14:34:19 -0700

    console logging in client.js

--- src/services/client.js
+++ src/services/client.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+console.log(  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY);
 export const client = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_KEY
