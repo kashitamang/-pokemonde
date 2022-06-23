9f6c75625282bb4766bdfa7e5973dbef69d25bd4
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-23 14:40:05 -0700

    pushing in the real terminal

--- src/services/client.js
+++ src/services/client.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-console.log(  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_KEY);
+console.log(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
 export const client = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_KEY
