68f66b33cc11c41a71e582133a72b499991ecc6d
kashitamang <100063861+kashitamang@users.noreply.github.com>
2022-06-23 12:42:31 -0700

    Revert "Dev"

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React from 'react';
-import logo from './pokemondelogo.jpg';
+import logo from './logo.svg';
+import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-
-
-import PokemondePage from './PokemondePage.js';
-import PokemonDetail from './PokemonDetail.js';
-
-export default function App() {
+function App() {
   return (
-    <Router>
-      <div className='App'>
-        <header>
-          <img src={logo} className="logo"/>
-        </header>
-        <Switch>
-          <Route exact path="/">
-            {/* this pokemonde page route should list all the pokemons */}
-            <PokemondePage />
-          </Route>
-          <Route exact path="/pokemons/:id"> 
-            {/* this route should point to a particular pokemon by id and render that specific PokemonDetail page */}
-            <PokemonDetail />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
